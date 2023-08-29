cf1315f5441e95dfd87c33c0b3715a6407c6f9e2
Roman-sleep <Roman06182000@yandex.ru>
2023-08-29 10:07:35 +0300

    infra 1.0

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from recipes.models import (Tag, Ingredient, Recipe,
-                            Favorites, ShoppingList)
+                            Favorites, ShoppingList, RecipeIngredient)
 from users.models import User
 from rest_framework.serializers import SerializerMethodField
 from rest_framework.exceptions import ValidationError
@@ -53,11 +53,71 @@ class IngredientSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'quantity')
 
 
+class RecipeIngredientSerializer(serializers.ModelSerializer):
+    ''' Сериализатор связи ингридиентов и рецепта/'''
+    id = serializers.PrimaryKeyRelatedField(
+        queryset=Ingredient.objects.all()
+    )
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = RecipeIngredient
+        fields = ('id', 'name', 'measurement_unit', 'amount',)
+
+
 class RecipeSerializer(serializers.ModelSerializer):
     '''Сериализатор модели Recipe.'''
+
+    tags = TagSerializer(many=True, read_only=True)
+    author = UserSerializer(read_only=True)
+    ingredients = SerializerMethodField()
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+
     class Meta:
         model = Recipe
-        fields = ('id', 'title', 'image', 'cooking_time')
+        fields = ('id', 'tags', 'autor', 'ingredients', 'title', 'image',
+                  'cooking_time', 'is_favorited', 'is_in_shopping_cart', 
+                  'description')
+
+    def get_ingredients(self, obj):
+        '''Cписок ингридиентов для рецепта.'''
+        ingredients = RecipeIngredient.objects.filter(recipe=obj)
+        return RecipeIngredientSerializer(ingredients, many=True).data
+
+    def get_is_favorited(self, obj):
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
+            return False
+        return obj.favorites.filter(user=request.user).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        request = self.context.get('request')
+        if not request or request.user.is_anonymous:
+            return False
+        return obj.shopping_list.filter(user=request.user).exists()
+
+    def create(self, validated_data):
+        '''Создание рецепта.'''
+        request = self.context.get('request', None)
+        tags = validated_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(author=request.user, **validated_data)
+        recipe.tags.set(tags)
+        self.create_ingredients(recipe, ingredients)
+        return recipe
+
+    def update(self, instance, validated_data):
+        '''Редактирование рецепта.'''
+        instance.tags.clear()
+        RecipeIngredient.objects.filter(recipe=instance).delete()
+        instance.tags.set(validated_data.pop('tags'))
+        ingredients = validated_data.pop('ingredients')
+        self.create_ingredients(instance, ingredients)
+        return super().update(instance, validated_data)
 
 
 class FavoritesSerializer(serializers.ModelSerializer):
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -62,10 +62,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'django'),
-        'USER': os.getenv('POSTGRES_USER', 'django'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', ''),
+        # 'HOST': 'localhost',
         'PORT': os.getenv('DB_PORT', 5432)
     }
 }
