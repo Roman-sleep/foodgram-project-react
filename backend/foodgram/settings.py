75a0f9c0f54ebe005e25bcbd0a2d44ad0110a922
Roman-sleep <Roman06182000@yandex.ru>
2023-09-07 11:41:10 +0300

    nginx.conf 3.4

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'my-default-secret-key')
 
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 INSTALLED_APPS = [
