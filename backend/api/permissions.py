26d03e7cebb38a6b047a1ca07b7f82ee6eb5308e
Roman-sleep <Roman06182000@yandex.ru>
2023-09-09 02:17:59 +0300

    v 4.9

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -5,6 +5,6 @@ class AuthorPermission(BasePermission):
     '''Делаем так, чтобы изменять и добавлять объекты
        мог только их автор'''
 
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, obj):
         return (request.method in SAFE_METHODS
                 or obj.author == request.user)
